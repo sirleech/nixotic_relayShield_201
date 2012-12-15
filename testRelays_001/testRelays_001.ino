f4ef9dc8b996029b59a9ee9bf7b9e3d1e68bc436
Chris Lee <sirleech@gmail.com>
2012-12-15 16:09:25 +1100

    SRS-001 update comments

--- testRelays_001/testRelays_001.ino
+++ testRelays_001/testRelays_001.ino
@@ -1,11 +1,6 @@
 // Test: SRS-001
 // Interface: Digital 5v
-// Description: Turn on and off relay 1 and 2
-// Success Criteria: A switched voltage is detected when relay is on
-// Notes: 	- Log failures to serial port
-//		- Green and red leds
-//		- Green = passing
-//		- Red = failure"
+// Description: Basic functions, logging
 
 void setup()
 {
